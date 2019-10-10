140f3e2d50a73a9ab36efbb4a4fdf45d5c841083
Andrzej Nowikowski <nowikowskia@student.mini.pw.edu.pl>
2019-10-10 13:50:15 +0200

    update

--- Wyjściówki/W1/gr1/NowikowskiAndrzej/source.R
+++ Wyjściówki/W1/gr1/NowikowskiAndrzej/source.R
@@ -1,22 +1,41 @@
 
 
-student <- function(id,grades, ...) {
+
+
+student <- function(id,grades) {
   # ... for future reference
-  if (!is.character(id)) stop("Id must be string")
-  if (!is.list(grades)) stop("Grades must be named list")
+  if (!validate_id(id)) stop("Id must be string")
+  if (!validate_grades(grades)) stop("Invalid grades")
+  value <- list(id=id, grades=grades)
+  attr(value, "class") <- "student"
+  value
+}
+
+validate_id <- function(id) {
+  return(is.character(id))
+}
+
+validate_grades <- function(grades) {
   for (i in 1:10) {
     p_name <- paste0('P', i)
     if (!(p_name %in% names(grades))) {
-      stop(paste0("Grade for ", p_name, " is missing"))
+      return(FALSE);
     }
     if (length(grades) != 10) {
-      stop("Grades for unknown P were given")
+      return(FALSE);
     }
   }
-  value <- list(id=id, grades=grades)
-  
-  attr(value, "class") <- "student"
-  value
+  return(TRUE)
+}
+
+validator <- function(student) {
+  if (!validate_id(student$id)) {
+    return(FALSE);
+  }
+  if (!validate_grades(student$grades)) {
+    return(FALSE);
+  }
+  return(TRUE);
 }
 
 s <- student("42", c(P1=5,P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=4))
@@ -24,12 +43,14 @@ s <- student("42", c(P1=5,P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=4))
 s <- student(42, c(P1=5,P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=4))
 s <- student("42", c(P1=5,P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=4, P22=4))
 s <- student("42", c(P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=4))
+s <- student("42", c(P1=5,P2=4,P3=5,P4=4,P5=4,P6=5,P7=5,P8=4,P9=5,P10=NA))
 s <- student("42", "xd")
 s <- student("42", "xd", "xd")
-
+s$grades
+validator(s)
 
 mean.student <- function(x, ...) {
-  mean(x$grades)
+  mean(x$grades, na.rm=TRUE)
 }
 
 mean(s)
