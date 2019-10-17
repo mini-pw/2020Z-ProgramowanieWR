b6a92b7ff9146b566946f795e5c588c5c7dc7a82
michbur <michalburdukiewicz@gmail.com>
2019-10-17 18:05:12 +0200

    added hallo

--- Materiały/L3/python/R/hello.R
+++ Materiały/L3/python/R/hello.R
@@ -11,3 +11,14 @@
 hello <- function(txt = "world") {
     cat("Hello, ", txt, "\n")
 }
+
+#' @title Hallo function
+#' 
+#' @inheritParams hello
+#' @details More informations.
+#' @seealso \code{\link[base]{cat}}
+#' @author Michal
+#' @export
+hallo <- function(txt = "world") {
+  cat("Hallo, ", txt, "\n")
+}
