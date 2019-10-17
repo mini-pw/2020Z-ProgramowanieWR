d48bc2a48daca9ada538a6676327ccc2e22a281d
michbur <michalburdukiewicz@gmail.com>
2019-10-17 17:58:53 +0200

    added more examples

--- Materiały/L3/python/R/hello.R
+++ Materiały/L3/python/R/hello.R
@@ -1,6 +1,10 @@
 #' @title Hello function
 #' 
 #' @param txt \code{character} vector
+#' @details More informations.
+#' @seealso \code{\link[base]{cat}}
+#' @author Michal
+#' @export
 hello <- function(txt = "world") {
     cat("Hello, ", txt, "\n")
 }
