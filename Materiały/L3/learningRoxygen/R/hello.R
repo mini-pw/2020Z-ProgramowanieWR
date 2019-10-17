bbb8c859a8c491ba137071bd5d04486909ac1572
michbur <michalburdukiewicz@gmail.com>
2019-10-17 13:09:02 +0200

    updated roxygen

--- Materiały/L3/learningRoxygen/R/hello.R
+++ Materiały/L3/learningRoxygen/R/hello.R
@@ -1,5 +1,22 @@
-
-## a placeholder
+#' @title Hello  function
+#' 
+#' @param txt an object of \code{character} class
+#' @importFrom dplyr %>%
+#' @export
+#' @examples
+#' hello("Michal")
 hello <- function(txt = "world") {
-    cat("Hello, ", txt, "\n")
+  paste0("Hello, ", txt) %>% 
+    cat("\n")
+}
+
+
+
+#' @title Hello/witam  function
+#' 
+#' @inheritParams hello
+#' @export
+hello_witam <- function(txt = "world") {
+  paste0("Hello/Witam, ", txt) %>% 
+    cat("\n")
 }
