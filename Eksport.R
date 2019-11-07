bed3c6702d75c3b648906f78d45015bd5b1f3c24
MrDomani <morgenp@student.mini.pw.edu.pl>
2019-11-07 18:15:54 +0100

    Wyjsciowka2

--- Eksport.R
+++
@@ -1,7 +0,0 @@
-NSE_factory <- function(FUNCTION){
-  out <- function(argslist, ...){
-    given_args <- quos(...)
-    lapply(given_args, function(expr) eval(quo_squash(expr), argslist)) -> evaluated_args
-    do.call(FUNCTION, evaluated_args)
-  }
-}
