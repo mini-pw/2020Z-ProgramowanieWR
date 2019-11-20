86d0df26e7d7226657779a20a9ac66702969f5ac
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-20 23:27:47 +0100

    PD2: nse_funs: remove debug prints

--- PraceDomowe/PD2/SlowikJadwiga/nse_funs.R
+++ PraceDomowe/PD2/SlowikJadwiga/nse_funs.R
@@ -2,8 +2,6 @@ library(rlang)
 
 my_NSE <- function(fun) {
   function(data, ...) {
-    print("nse")
-    print(data)
     dots <- enquos(...)
     args <- lapply(dots, function(arg) {
       eval_tidy(arg, data=data)
@@ -14,8 +12,6 @@ my_NSE <- function(fun) {
 
 my_NSE2 <- function(fun) {
   function(data, ...) {
-    print("nse2")
-    print(data)
     dots <- exprs(...)
     args <- purrr::map(dots, function(arg) eval_tidy(arg, data))
     eval_tidy(expr(fun(!!!args)))
@@ -24,8 +20,6 @@ my_NSE2 <- function(fun) {
 
 my_NSE3 <- function(fun) {
   function(data, ...) {
-    print("nse3")
-    print(data)
     dots <- enquos(...)
     args <- purrr::map(dots, function(arg) eval_tidy(arg, data))
     eval_tidy(call2(expr(fun), !!!args))
@@ -34,8 +28,6 @@ my_NSE3 <- function(fun) {
 
 my_NSE4 <- function(fun) {
   function(data, ...) {
-    print("nse4")
-    print(data)
     dots <- as.list(substitute(list(...)))[-1]
     args <- purrr::map(dots, function(arg) eval(arg, data))
     do.call(fun, args)
