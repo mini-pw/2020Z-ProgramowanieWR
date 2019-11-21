70c9e4e190865e89eaa590d99384f92c19453c92
zuzannamagierska <magierskaz@student.mini.pw.edu.pl>
2019-11-21 18:53:06 +0100

    fix

--- Wyjsciowki/W3/gr2/MagierskaZuzanna/shiny-js/app.R
+++ Wyjsciowki/W3/gr2/MagierskaZuzanna/shiny-js/app.R
@@ -1,56 +1,63 @@
 # adapted from https://gist.github.com/garrettgman/6673251
 
 library(shiny)
-library(shinycssloaders)
-library(shinyhelper)
 
-f_names <- c("numericInput", "selectInput", "sliderInput")
-l <- lapply(f_names, function(ith_f_name) 
-    function(inputId, ...) 
-        helper(getFromNamespace(ith_f_name, ns = "shiny")(inputId = inputId, ...),  
-               content = inputId, type = "markdown")
+plotOutputDownload <- function(name) {
+    fluidRow(
+        plotOutput(name),
+        downloadButton(outputId = paste0(name, "Download"), label = "Download the plot")
     )
-
-for(i in 1L:length(f_names)) {
-    assign(x = paste0(f_names[i], "Helper"), value = l[[i]])
 }
-
 
 ui <- pageWithSidebar(
     headerPanel("Interactive Histogram"),
     sidebarPanel(
-        numericInputHelper("n", "Generate this many points", 
-                     min = 1, value = 10e6),
-        selectInputHelper("family", "From this family",
-                    choices = c("Normal",
-                                "Uniform",
-                                "Exponential"),
-                    selected = "normal"),
-        sliderInputHelper("bins", "number of bins", 
-                    min = 1, max = 100, value = 50)
+        numericInput("lambda", "Lambda", value=10)
     ),
     mainPanel(
-        withSpinner(helper(plotOutput("histogram"), content = "plot", type = "markdown"))
+        plotOutputDownload("histogram"),
+        plotOutputDownload("density")
     )
 )
 
 
 server <- function(input, output) {
     
-    observe_helpers(withMathJax = TRUE, help_dir = "markdowny_pomocowe")
-    
     data <- reactive({ 
-        FUN <- switch(input[["family"]],
-                      "Normal" = rnorm,
-                      "Uniform" = runif,
-                      "Exponential" = rexp)
-        FUN(input[["n"]])
+        rpois(1000, input[["lambda"]])
+    })
+    
+    histogram_plot <- reactive({
+        hist(data())
+    })
+    
+    density_plot <- reactive({
+        density(data())
     })
     
     output[["histogram"]] <- renderPlot({
-        hist(data(), breaks = input$bins)
+        histogram_plot()
     })
     
+    output[["density"]] <- renderPlot({
+        plot(density_plot())
+    })
+    
+    output[["histogramDownload"]] = downloadHandler(
+        filename = 'histogram.png',
+        content = function(file) {
+            png(file)
+            plot(histogram_plot())
+            dev.off()
+        })
+    
+    output[["densityDownload"]] = downloadHandler(
+        filename = 'density.png',
+        content = function(file) {
+            png(file)
+            plot(density_plot())
+            dev.off()
+        })
 }
 
 # Run the application 
