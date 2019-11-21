9e124598ac4db55fc5768252ab573ba07898e33e
michbur <michalburdukiewicz@gmail.com>
2019-11-21 17:15:49 +0100

    updated shinyjs

--- Materialy/L6/shiny-js/app.R
+++ Materialy/L6/shiny-js/app.R
@@ -1,26 +1,13 @@
 # adapted from https://gist.github.com/garrettgman/6673251
 
 library(shiny)
-library(shinycssloaders)
-library(shinyhelper)
-
-func_vec <- c("numericInput", "selectInput", "sliderInput")
-func_list <- lapply(func_vec, function(ith_fun) 
-    function(inputId, ...) {
-        helper(getFromNamespace(ith_fun, ns = "shiny")(inputId = inputId, ...),  
-               content = inputId, type = "markdown")
-    })
-
-for(ith_fun_id in 1L:length(func_list)) {
-    assign(x = paste0(func_vec[ith_fun_id], "Helper"), value = func_list[[ith_fun_id]])
-}
 
 ui <- pageWithSidebar(
     headerPanel("Interactive Histogram"),
     sidebarPanel(
-        numericInputHelper("n", "Generate this many points", 
+        numericInput("n", "Generate this many points", 
                      min = 1, value = 10e6),
-        selectInputHelper("family", "From this family",
+        selectInput("family", "From this family",
                     choices = c("Normal",
                                 "Uniform",
                                 "Exponential"),
@@ -29,13 +16,12 @@ ui <- pageWithSidebar(
                     min = 1, max = 100, value = 50)
     ),
     mainPanel(
-        helper(withSpinner(plotOutput("histogram")), content = "example-file", type = "markdown")
+        plotOutput("histogram")
     )
 )
 
 
 server <- function(input, output) {
-    observe_helpers(help_dir = "help_mds")
     
     data <- reactive({ 
         FUN <- switch(input[["family"]],
