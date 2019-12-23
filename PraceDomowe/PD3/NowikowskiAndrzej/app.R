23334515df03989711d7fafdb9e716a6835c7698
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2019-12-23 19:01:23 +0100

    scatter plot

--- PraceDomowe/PD3/NowikowskiAndrzej/app.R
+++ PraceDomowe/PD3/NowikowskiAndrzej/app.R
@@ -8,41 +8,80 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Interactive plotter"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        fileInput("csv_file",
+                  "Please select a .csv file",
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")
+        ),
+        selectInput("plot_type",
+                  "Please select a plot type", choices=c("barplot", "scatterplot", "heatmap")
+        ),
+        uiOutput("plot_controls")
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("plot")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  csv <- reactive({
+    validate(
+      need(input$csv_file, "Please select .csv file")
+    )
+    as.data.frame(read.csv(input$csv_file$datapath))
+  })
+  
+  output$plot_controls <- renderUI({
+    required_variables <- colnames(csv())
+    optional_variables <- c('None', required_variables)
+    if (input$plot_type == "scatterplot") {
+      tagList(
+        selectInput("x_variable", "X-axis", choices=required_variables),
+        selectInput("y_variable", "Y-axis", choices=required_variables),
+        selectInput("color", "Color", choices=optional_variables),
+        selectInput("facet", "Facet", choices=optional_variables)
+      )
+    }
+  })
+  
+  output$plot <- renderPlot({
+    df <- csv()
+
+    aes <- aes_string(
+      x=input$x_variable, 
+      y=input$y_variable,
+      color=input$color
+    )
+    if (input$color == 'None') {
+      aes$colour <- NULL
+    }
+  
+    plot <- ggplot(df, aes)
+    
+    if (input$plot_type == 'scatterplot') {
+      plot <- plot + geom_point()
+    }
+    
+    if (input$facet != 'None') {
+      plot <- plot + facet_wrap(c(input$facet))
+    }
+    plot
+  })
 }
 
 # Run the application 
